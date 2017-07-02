79614e6914d0a19fad686e7ef0f97582777d416b
Adsavin <adsavin@live.com>
2017-07-02 17:01:01 +0700

    no message

--- views/site/index.php
+++ views/site/index.php
@@ -80,7 +80,7 @@ $map = new \dosamigos\google\maps\Map([
                                     <div class="card-content">
                                         <div class="media">
                                             <div class="media-left">
-                                                <figure class="image is-128x128 is-hidden-desktop">
+                                                <figure class="image is-64x64 is-hidden-desktop">
                                                     <img src="upload/photo/<?= $product->photo ?>" alt="Image">
                                                 </figure>
                                             </div>
